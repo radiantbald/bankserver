9c4ac5ae5a2827bf1defa97b71def1b4600af2fb
Oleg Popov <popov.oleg26@wb.ru>
2025-06-08 16:10:57 +0300

    Правки auth

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"bankserver/auth"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-
 func main() {
 	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-
+	router.POST("/hello/:name", auth.CreateUser)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
